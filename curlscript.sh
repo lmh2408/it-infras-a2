a915f4f30ed790762a6bd3efbeb0ada8aaeac343
zero <noemail>
2020-06-05 05:14:42 +1000

    5

--- curlscript.sh
+++ curlscript.sh
@@ -1,6 +1,7 @@
 # ec2-54-166-36-5.compute-1.amazonaws.com
 # /register
-
+# 'postgres://postgres:T46750p0mxfkS99TbDoU@database-1.czmvwofht5pq.us-east-1.rds.amazonaws.com:5432/'
 curl -X POST --data "username=mra" --data "password=123456" https://ec2-54-166-36-5.compute-1.amazonaws.com/register
 
 curl -X POST --data "username=mra&password=123456" ec2-54-166-36-5.compute-1.amazonaws.com/register
+
